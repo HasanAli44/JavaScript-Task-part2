44a7cd8fcfea80c62e4cd641b90f3e9e9acb3765
HasanAli44 <mdhasanali8439@gmail.com>
2024-09-26 11:50:11 +0600

    findaveragephoneprice and return result

--- README.md
+++ README.md
@@ -52,3 +52,28 @@ function electronics(laptopQuentity, tableQuentity, mobileQuentity) {
 console.log(electronics(2, 5, 8));
 
 ```
+
+## 4.Find average price of phone and return result:
+
+```
+// task-4
+function findAveragePhonePrice(phones) {
+  let sum = 0;
+  for (let phone of phones) {
+    sum = sum + phone.price;
+  }
+  const average = sum / phones.length;
+
+  return Math.round(average);
+}
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+console.log(findAveragePhonePrice(phones));
+```
--- task.js
+++ task.js
@@ -9,7 +9,7 @@ function findLowest(numbers) {
   return lowNumber;
 }
 const heights2 = [167, 190, 120, 165, 137, 133, 105, 100, -20];
-console.log(heights2);
+// console.log(heights2);
 //
 
 // task-2
@@ -24,7 +24,7 @@ function findFriend(names) {
 }
 const heights3 = ["rafi", "ron", "rashed", "o", "rahim", "robin", "t"];
 
-console.log(findFriend(heights3));
+// console.log(findFriend(heights3));
 
 // task-3
 function electronics(laptopQuentity, tableQuentity, mobileQuentity) {
@@ -35,7 +35,28 @@ function electronics(laptopQuentity, tableQuentity, mobileQuentity) {
   const tabletCost = tabletPrice * tableQuentity;
   const mobileCost = mobilePrice * mobileQuentity;
   const totalCost = laptopCost + tabletCost + mobileCost;
-  console.log(totalCost);
+  //   console.log(totalCost);
 }
 
-console.log(electronics(2, 5, 8));
+// console.log(electronics(2, 5, 8));
+
+// task-4
+function findAveragePhonePrice(phones) {
+  let sum = 0;
+  for (let phone of phones) {
+    sum = sum + phone.price;
+  }
+  const average = sum / phones.length;
+
+  return Math.round(average);
+}
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+console.log(findAveragePhonePrice(phones));

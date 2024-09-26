fa52ab682b62c7b5efa738bdfc794ebf234e74af
HasanAli44 <mdhasanali8439@gmail.com>
2024-09-26 11:01:23 +0600

    find smallest name

--- task.js
+++ task.js
@@ -9,4 +9,18 @@ function findLowest(numbers) {
   return lowNumber;
 }
 const heights2 = [167, 190, 120, 165, 137, 133, 105, 100, -20];
-console.log(findLowest(heights2));
+// console.log(findLowest(heights2));
+
+// task-2
+function findFriend(names) {
+  let smallestName = names[0];
+  for (let nam of names) {
+    if (nam.length < smallestName.length) {
+      smallestName = nam;
+    }
+  }
+  return smallestName;
+}
+const heights3 = ["rafi", "ron", "rashed", "o", "rahim", "robin", "t"];
+
+console.log(findFriend(heights3));
